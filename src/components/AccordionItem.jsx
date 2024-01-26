3b0f022d4456c6b10fd4d74bbfadbc353f5de200
Christian <habinezac15@gmail.com>
2024-01-26 08:10:41 +0200

    ft: project completed

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 
-const AccordionItem = ({ num, title, text }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleToggle = () => {
-    setIsOpen((isOpen) => !isOpen);
+const AccordionItem = ({ num, title, text, currOpen, onOpen }) => {
+  const isOpen = num === currOpen;
+  const handleToggle = (num) => {
+    isOpen ? onOpen(0) : onOpen(num);
   };
 
   return (
-    <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
+    <div
+      className={`item ${isOpen ? 'open' : ''}`}
+      onClick={() => handleToggle(num)}
+    >
       <p className="number">{num > 0 ? `0${num}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? '-' : '+'}</p>
